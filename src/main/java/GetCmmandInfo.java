24ac3f2dac307e6139fdeaac4f6dac504090b267
hujunxiang <hujunxiang@panda.tv>
2018-05-03 18:55:36 +0800

    修改获取命令的功能文件，只包含获取命令行信息和正则表达式函数通用方法

--- src/main/java/GetCmmandInfo.java
+++ src/main/java/GetCmmandInfo.java
@@ -39,19 +39,4 @@ public class GetCmmandInfo {
             return null;
         }
     }
-
-    public int  GetAndroidVersion() throws Exception {
-        GetCmmandInfo GC = new GetCmmandInfo("adb shell getprop ro.build.version.release");
-        String verinfo = GC.appInfo()[1];
-        String ver = GC.regStr(verinfo, "\\d?");
-        int androidVer = Integer.parseInt(ver);
-        return androidVer;
-    }
-
-    public String GetAppUserID() throws Exception {
-        GetCmmandInfo GC= new GetCmmandInfo("adb shell dumpsys package com.panda.videoliveplatform|grep userId");
-        String userIdinfo = GC.appInfo()[1];
-        return GC.regStr(userIdinfo, "\\d+");
-    }
-
 }
