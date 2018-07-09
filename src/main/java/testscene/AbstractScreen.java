83560c054533c3696d43311f8fce0d90988938e0
hjx19 <hjx19881022@126.com>
2018-07-10 00:03:32 +0800

    ioroomtestscene

--- src/main/java/testscene/AbstractScreen.java
+++ src/main/java/testscene/AbstractScreen.java
@@ -5,12 +5,12 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import static io.appium.java_client.touch.offset.PointOption.point;
 
 public abstract class AbstractScreen {
 
@@ -20,41 +20,14 @@ public abstract class AbstractScreen {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
-    /**
-     * description：定位到元素坐标，然后进行滑动操作
-     * elem: 要操作的元素     *
-     * moveX：从起始x位置需要移动的距离（移动到beginX+moveX的位置）
-     * moveY：从起始y位置需要移动的距离（移动到beginY+moveY的位置）
-     */
-    public void swipeToPosition(MobileElement elem,int moveX,int moveY){
-        //MobileElement elem = driver.findElement(By.id(id));
-        System.out.println("moveX ="+moveX);
-        System.out.println("moveY ="+moveY);
-        Point start = elem.getLocation();
+    public void swipeToPosition(MobileElement elem,int moveY){
         TouchAction ta = new TouchAction(this.driver);
-        int startX = start.x;
-        System.out.println("startX ="+startX);
-        int startY = start.y;
-        System.out.println("startY ="+startY);
-        // 获取控件坐标轴差
-        Dimension q = elem.getSize();
-        int x = q.getWidth();
-        System.out.println("x ="+x);
-        int y = q.getHeight();
-        System.out.println("y ="+y);
-        // 计算出控件结束坐标
-        int endX = x + startX;
-        System.out.println("endX ="+endX);
-        int endY = y + startY;
-        System.out.println("endY ="+endY);
-        // 计算中间点坐标
-        int centreX = (endX + startX) / 2;
-        System.out.println("centreX ="+centreX);
-        int centreY = (endY + startY) / 2;
-        System.out.println("centreY="+centreY);
-        ta.press(centreX,centreY).moveTo(moveX,moveY).release().perform();
+        Point ecenter = elem.getCenter();
+        int cx = ecenter.getX(); //971
+        int cy = ecenter.getY(); //1764
+        ta.press(point(cx,cy)).moveTo(point(cx,moveY)).release().perform();
     }
-//    public void outRoom
+
     public MobileElement findElementWithTimeout(By by, int timeOutInSeconds) {
         return (MobileElement)(new WebDriverWait(driver, timeOutInSeconds)).until(ExpectedConditions.presenceOfElementLocated(by));
     }
--- src/main/java/testscene/AllRoomScreen.java
+++ src/main/java/testscene/AllRoomScreen.java
@@ -6,24 +6,39 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 
 import java.util.List;
 
+
 public class AllRoomScreen extends AbstractScreen {
     @AndroidFindBy(id = "com.panda.videoliveplatform:id/tv_fans_count")
     private List<MobileElement> roomElements;
+    @AndroidFindBy(id ="com.panda.videoliveplatform:id/iv_chinajoy_close")
+    private MobileElement chinajoyclose;
     public AllRoomScreen(AppiumDriver<?> driver) {
         super(driver);
     }
 
     public void ioRoom() throws InterruptedException {
-        Thread.sleep(20000);
-        int mesize = roomElements.size();
-        for(MobileElement m :roomElements){
-            m.click();
-            mesize --;
-            if (mesize == 0){
-                this.swipeToPosition(m,0,-600);
+        Thread.sleep(7000);
+        int page = 10;
+        while (page != 0){
+            Thread.sleep(3000);
+            int mesize = roomElements.size();
+            for(MobileElement m :roomElements){
+                m.click();
+                Thread.sleep(2000);
+                if(chinajoyclose != null){
+                    driver.navigate().back();
+                }
+                driver.navigate().back();
+                System.out.println(mesize);
+                if (mesize == 1){
+                    this.swipeToPosition(m,167);
+                    System.out.println("move to y300");
+                }
+                mesize --;
             }
+            page  --;
         }
-
     }
+
 }
 
