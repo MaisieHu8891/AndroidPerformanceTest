2bac075abb45137337ec667d7c1e79d3965dc2fc
hujunxiang <hujunxiang@panda.tv>
2018-09-20 11:29:52 +0800

    修改ProcessBuilder参数为String[]

--- src/test/java/MiaoKaiTest.java
+++ src/test/java/MiaoKaiTest.java
@@ -3,10 +3,11 @@ import testscene.HandleData;
 import java.io.IOException;
 
 public class MiaoKaiTest{
-    public static void main(String[] args) throws InterruptedException, IOException {
-        HandleData d = new HandleData("D:\\TEST\\androidtestbuildapk\\logcat\\logcattest.txt");
-//        d.getdata();
-        String[] keys = {"miaokai test time except Network Connection","miaokai test total time"};
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String[] cmds = {"adb", "shell", "logcat" ,"|grep" ,"miaokai"};
+        HandleData d = new HandleData(cmds,"D:\\TEST\\androidtestbuildapk\\logcat\\","D:\\TEST\\androidtestbuildapk\\logcat\\logcattest.txt");
+        d.getdata();
+//        String[] keys = {"miaokai test time except Network Connection","miaokai test total time"};
 //        d.filterData(keys,"D:\\TEST\\androidtestbuildapk\\logcat\\logcat.csv");
     }
 }
